6a774fe2a08cb393c1d360dbb240b81cba5d46dc
emmanyouwell <emingala02@gmail.com>
2023-09-03 20:58:00 +0800

    Specifications

--- src/Computer/Main.java
+++ src/Computer/Main.java
@@ -9,9 +9,24 @@ public class Main {
 		//List of parts
 		ArrayList<Parts> parts = new ArrayList<Parts>();
 		
+		//List of Specifications
+		ArrayList<String> motherboardSpecs = new ArrayList<String>();
+		
+		motherboardSpecs.add("Manufacturer – ASOS");
+		motherboardSpecs.add("Socket - LGA1200");
+		motherboardSpecs.add("Chipset – INTOL H310");
+		motherboardSpecs.add("Form Factor – miniITX");
+		motherboardSpecs.add("RAM type – DDR4");
+		motherboardSpecs.add("Max RAM size – 64 GB");
+		motherboardSpecs.add("RAM slots count – 2");
+		motherboardSpecs.add("SATA slots count – 4");
+		motherboardSpecs.add("M2 slots count – 2");
+		
+		
+		
 		//Individual parts
-		Parts pcCase = new Parts("Pc Case");
-		Parts motherboard = new Parts("Motherboard");
+		Parts pcCase = new Parts("Pc Case", false, pcCaseSpecs);
+		Parts motherboard = new Parts("Motherboard", motherboardSpecs);
 		
 		
 		
@@ -29,14 +44,45 @@ public class Main {
 		for (Parts p : pc1.getParts()) {
 			//Check for broken parts and print it.
 			
-			System.out.println(p.getName());
+			System.out.println(p.getName() + p.checkPart());
 		}
-		
+		System.out.println();
 		//Print the price
+		System.out.println(pc1.getPrice());
+		System.out.println();
 		
+	
 		
 		//Then, fix it.
+		pc1.getParts().get(0).fixPart();
 		
+		//pc1 : Object
+		//pc1.getParts() : ArrayList<Parts> parts
+		//parts.get(0) : Parts pc_case
+		//pc_case.fixPart()
+		
+		
+		//Print the parts again
+		for (Parts p : pc1.getParts()) {
+			//Check for broken parts and print it.
+			
+			System.out.println(p.getName() + p.checkPart());
+		}
+		System.out.println();
+		
+		//Print specifications
+		
+//		for (Parts p : pc1.getParts()) {
+//			System.out.println(p.getName() + " specifications:");
+//			if (p.getSpecs().isEmpty()) {
+//				System.out.println("None");
+//			}
+//			else {
+//				p.printSpecs();
+//			}
+//			
+//			
+//		}
 		
 		
 	}
--- src/Computer/Parts.java
+++ src/Computer/Parts.java
@@ -1,13 +1,17 @@
 package Computer;
 
+
+import java.util.List;
+
 public class Parts {
 	private String name;
 	private boolean isFunctional;
+	private List<String> specs;
 	
+	//Constructors
 	Parts(){
 		setFunctional(true);
 	}
-	
 	Parts(String name){
 		setName(name);
 		setFunctional(true);
@@ -15,7 +19,39 @@ public class Parts {
 	Parts(String name, boolean isFunctional){
 		setName(name);
 		setFunctional(isFunctional);
+		
 	}
+	Parts(String name, List<String> specs){
+		setName(name);
+		setSpecs(specs);
+		setFunctional(true);
+	}
+	Parts(String name, boolean isFunctional, List<String> specs){
+		setName(name);
+		setFunctional(isFunctional);
+		setSpecs(specs);
+		
+	}
+	//End of Constructor
+	
+	public String checkPart() {
+		String msg="";
+		if (isFunctional()) {
+			msg = " (Functional)";
+		}
+		else {
+			msg = " (Broken)";
+		}
+		return msg;
+	}
+	
+	public void printSpecs() {
+		for (String item : getSpecs()) {
+			System.out.println(item);
+		}
+	}
+	
+	
 	
 	
 	
@@ -23,7 +59,13 @@ public class Parts {
 		setFunctional(true);
 	}
 	
-	
+	public List<String> getSpecs() {
+		return specs;
+	}
+
+	public void setSpecs(List<String> specs) {
+		this.specs = specs;
+	}
 	public String getName() {
 		return name;
 	}
